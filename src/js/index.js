db403fe81fbf353a86cff6dc965ae4f7fade4938
SenpaiPombo <124484930+SenpaiPombo@users.noreply.github.com>
2025-04-15 22:31:56 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const copyButton = document.getElementById("copyButton");
+
+copyButton.addEventListener("click", () => {
+  // Selecionar o texto que você quer copiar (por exemplo, de um elemento com id="textoACopiar")
+  const textoACopiar = document.getElementById("textoACopiar").textContent;
+
+  // Copiar o texto para a área de transferência
+  navigator.clipboard.writeText(textoACopiar)
+    .then(() => {
+      // Opcional: exibir uma mensagem ao usuário
+      alert("Pix copiado com sucesso! (pix do meu filho Rafael da Silva Camozzato). Por favor verificar o nome antes de tranferir");
+    })
+    .catch((error) => {
+      // Opcional: lidar com erros, se necessário
+      console.error("Erro ao copiar para a área de transferência:", error);
+    });
+});
